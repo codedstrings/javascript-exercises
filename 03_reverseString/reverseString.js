d92857e89d60dff8af4234985389cb5d6bae85b8
codedstrings <mridhulml@gmail.com>
2024-01-02 14:35:33 +0530

    completed: 03_reversestring

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let tempArr=str.split('');
+    tempArr.reverse();
+    return tempArr.join('');
 };
 
 // Do not edit below this line
