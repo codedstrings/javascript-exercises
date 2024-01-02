0be075db89fd31406e6d724b92c145e9053fc20e
codedstrings <mridhulml@gmail.com>
2024-01-02 14:28:17 +0530

    completed: 02_repeatstring

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeatVal) {
+    if(repeatVal<0) return "ERROR";
+    let returnStr='';
+    for (let index = 0; index < repeatVal; index++) {
+        returnStr=returnStr.concat(str);
+    }
+    return returnStr;
 };
 
 // Do not edit below this line
