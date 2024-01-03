e211f1d3a17edde0a549cf1d2cdbbac235f18967
codedstrings <mridhulml@gmail.com>
2024-01-03 17:51:10 +0530

    completed: 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    let start, end;
+    if (typeof (num1) != "number" || typeof (num2) != "number") return "ERROR";
+    //alternatively
+    // if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR"
+    }
+    else if (num1 < num2) {
+        start = num1;
+        end = num2;
+    }
+    else {
+        start = num2;
+        end = num1;
+    }
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
