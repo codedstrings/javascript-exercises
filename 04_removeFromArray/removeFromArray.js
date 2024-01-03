b85e2748dcc6c6b91bf9402b7fb84624df97b147
codedstrings <mridhulml@gmail.com>
2024-01-03 17:15:58 +0530

    completed:04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+    //iterate throught the arguments array using i
+    //delete one item per loop iteration.
+    for (let i in args) {
 
+        let index = arr.indexOf(args[i]);
+        //check if value to delete is present in the array. -1 means not present.
+        if (index != -1) {
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
