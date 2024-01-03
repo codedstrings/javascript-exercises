147db90aadb4a69afa9fd4cb93a0af6b339ef085
codedstrings <mridhulml@gmail.com>
2024-01-03 21:30:54 +0530

    completed: 06_leapyears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,23 @@
-const leapYears = function() {
+const leapYears = function (year) {
+
+    // if (year % 100 == 0) {
+    //     if (year % 400 == 0) return true;
+    //     else return false;
+    // }
+    // else if (year % 4 == 0) return true;
+    // else return false;
+    
+    //this also works, it's an expansion of the solution given.
+    if(year%4==0){
+        if(year%100==0){
+            if(year%400==0){
+                return true;
+            }
+            else return false;
+        }
+        else return true;
+    }
+    else return false;
 
 };
 
