f6d53c22b8330d7fc2b42299adbe83c95d676ce1
codedstrings <mridhulml@gmail.com>
2024-01-20 23:50:45 +0530

    completed: 09_palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // let regex=/[a-z0-9]/g;
+    let regex=/\w/g;
+    let processedString=string.toLowerCase().match(regex).join("");
+    return processedString.split("").reverse().join("") == processedString;
 };
+console.log(palindromes('malayalamm'));
 
 // Do not edit below this line
 module.exports = palindromes;
