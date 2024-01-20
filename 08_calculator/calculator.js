ef49b3b7feba2fcd5a5d63af7e3de5e027602b8f
codedstrings <mridhulml@gmail.com>
2024-01-20 22:57:35 +0530

    completed: 08_calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,43 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numArr) {
+  // let sum=0;
+  // for(let i in numArr){
+  //   sum+=numArr[i];
+  // }
+  // return sum;
+  return numArr.reduce((sum, val) => {
+    return (sum += val);
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((product, val) => {
+    return (product *= val);
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (a,b) {
+  return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let fact=1,i=2;
+  //loop doesn't run if num=1 or num=0; fact is returned as 1
+  while(i<=num){
+      // console.log("pass"+i+" | "+fact+" * "+i);
+    fact=fact*i;
+    i++;
+  }
+  return fact;
 };
+console.log("hello")
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +46,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
