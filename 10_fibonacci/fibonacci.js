362d636561eaf144647258ac8241c852594e3bb6
codedstrings <mridhulml@gmail.com>
2024-01-22 10:27:55 +0530

    completed: 10_fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(val) {
+    if(val<0){
+        return "OOPS"
+    }
+    let fibonacci=0; prev=1; temp=0;
+    for(let i=0 ;i<val;i++){
+          console.log("prev="+prev+" ,fibonacci="+fibonacci);
+          temp=fibonacci;
+        fibonacci=fibonacci+prev;
+        prev=temp;
+        console.log("after: prev="+prev+" ,fibonacci="+fibonacci);
+      
+    }
+    return fibonacci;
 };
+
+console.log("final: "+fibonacci(4))
 
 // Do not edit below this line
 module.exports = fibonacci;
